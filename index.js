c93448e17960208cda907d0b5cb18e40ccf33647
Aidan <asmaidan@look.com>
2019-07-30 22:19:22 -0500

    Json parse fix

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const JSON = require('json')
 const Room = require('ipfs-pubsub-room')
 const IPFS = require('ipfs')
 const ipfs = new IPFS({
@@ -19,19 +20,13 @@ ipfs.on('ready', () => {
   // the pubsub room is like a radio station 
   const room = Room(ipfs, 'random name you come up with')
 
-  // Youtube api to get details about music but this can be any api as well
-  const ytlist = require('youtube-playlist');
-  const url = 'https://www.youtube.com/playlist?list=PLmyFRwzYRiBdBKi6ftBFZiT3e_QwA8fAP';
  
   // when a listener joins the room this function will then
   // broadcast the music deatails as json to the station/room 
   // to simulate multiple people open this repl in an incognito tab or device
   room.on('peer joined', (peer) => {
-    ytlist(url, ['id', 'name', 'url']).then(res => {
-    //console.log(res.data); 
-    room.broadcast('${res.data}');
-
-  });
+    
+    room.broadcast('Hello from macbook');
     console.log('New listener', peer)
   })
  
@@ -41,7 +36,10 @@ ipfs.on('ready', () => {
 
 
   room.on('message', (message) => {
-    console.log(message)
+    const data = JSON.parseLookup(message.data)
+    //const msg = JSON.parse(data)
+    console.log(message.data.toString())
+    //console.log(data)
   })
 
 
